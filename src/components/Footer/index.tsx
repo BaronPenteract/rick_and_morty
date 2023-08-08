0eb56483a2128442bec38877b4d533e7e3c4a7c2
BaronPenteract <Andreas42R@yandex.ru>
2023-08-08 18:04:04 +0300

    footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,48 +1,81 @@
-import React from "react";
-
-import { motion, Variants } from "framer-motion";
+import React, { ReactElement } from "react";
 
 import ApiLink from "../ApiLink";
 import styles from "./index.module.scss";
 import GitHubLink from "../GitHubLink";
 import { PROJECT_TITLE, rootPath } from "../../utils/constants";
-import { Link } from "react-router-dom";
-
-const footerAnimation: Variants = {
-  hidden: {
-    scaleY: 0,
-    opacity: 1,
-    transition: {
-      ease: "linear",
-      duration: 2.5,
-      type: "tween",
-    },
-  },
-  visible: {
-    scaleY: 1,
-    opacity: 1,
-    transition: {
-      duration: 0.5,
-    },
-  },
-};
+import { Link, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useSelector } from "react-redux";
+import { INavItem } from "../../@types/mainComponent";
+import { getCharsSelector } from "../../redux/chars/selectors";
+import { navItems } from "../../utils/navItems";
+import { navButtonAnimation } from "../NavTab/animation";
+import { getEpisodesSelector } from "../../redux/episodes/selectors";
 
 const Footer: React.FC = () => {
+  const navigate = useNavigate();
+
+  const { favChars, charsCount } = useSelector(getCharsSelector);
+  const { episodesCount } = useSelector(getEpisodesSelector);
+
+  const handleClickNav: (navItem: INavItem) => void = (navItem) => {
+    navigate(navItem.to);
+  };
+
+  const navButtons: ReactElement<HTMLButtonElement>[] = [];
+  navItems.forEach((item, idx) => {
+    if (idx === 0) return;
+
+    navButtons.push(
+      <li>
+        <button
+          key={idx}
+          className={styles.button}
+          onClick={() => handleClickNav(item)}
+        >
+          <Link className={styles.buttonTitle} to={item.to}>
+            {item.title}
+          </Link>
+          {item.title.toLocaleLowerCase() === "favorite" &&
+          favChars.length > 0 ? (
+            <span className={styles.buttonNotify}>{favChars.length}</span>
+          ) : (
+            ""
+          )}
+        </button>
+      </li>
+    );
+  });
+
   return (
-    <motion.footer
-      className={styles.root}
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ amount: 0.6 }}
-    >
+    <footer className={styles.root}>
       <div className={styles.container}>
-        <div className={styles.content}>{PROJECT_TITLE}</div>
-        <div className={styles.info}>
+        <div className={styles.content}>
+          <nav className={`${styles.nav}`}>
+            <h3 className={styles.nav__title}>Navigation</h3>
+            <ul className={styles.nav__list}>{navButtons}</ul>
+          </nav>
+          <div className={styles.info}>
+            <h3 className={styles.info__title}>Statistic</h3>
+            <ul className={styles.info__list}>
+              <li>
+                <span>Total characters: </span>
+                <span>{charsCount}</span>
+              </li>
+              <li>
+                <span>Total episodes: </span>
+                <span>{episodesCount}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.contact}>
           <ApiLink />
           <GitHubLink />
         </div>
       </div>
-    </motion.footer>
+    </footer>
   );
 };
 
